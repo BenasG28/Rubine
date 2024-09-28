839bdda482c8fddf8ab08579c95ce2435ca8b849
BenasG28 <114530547+BenasG28@users.noreply.github.com>
2024-09-28 17:30:41 +0300

    Transition from MySQL to MariaDB, user changes.

--- back-end/src/main/java/com/rubine/BackEndApplication.java
+++ back-end/src/main/java/com/rubine/BackEndApplication.java
@@ -1,8 +1,6 @@
-package com.rubine.backend;
+package com.rubine;
 
-import com.rubine.controllers.UserController;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
--- back-end/src/main/java/com/rubine/model/User.java
+++ back-end/src/main/java/com/rubine/model/User.java
@@ -1,12 +1,8 @@
 package com.rubine.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Entity
 @Getter
@@ -17,19 +13,5 @@ public class User {
     private long id;
     private String name;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public User() {}
 }
